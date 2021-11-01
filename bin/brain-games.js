046fc86d7d73ee281bf71bda1170d49365ab07e1
VKozhura <kozhura_v@mail.ru>
2021-11-02 00:06:20 +0300

    установила eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import getAnswer from "../src/cli.js";
+import getAnswer from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 getAnswer();
